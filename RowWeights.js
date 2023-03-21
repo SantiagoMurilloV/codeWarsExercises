044ab3574db9c67870bf959f5b5d2b8694f3c731
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2023-03-21 01:16:56 -0500

    Add files via upload

---
+++ RowWeights.js
@@ -0,0 +1,18 @@
+function rowWeights(arr){ 
+        let wTeam1 = 0;
+        let wTeam2 = 0;
+        let newArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (i % 2 === 0) {
+                wTeam1 += arr[i]; //The += operator is used to 
+                                    // add the value on the right to the variable on the
+                                    //  left and assign the result to the variable on the left.
+            } else {
+                wTeam2 += arr[i];
+            }
+        }
+    newArr = [wTeam1, wTeam2];
+    console.log(newArr);
+}
+
+rowWeights([13, 27, 49]) ;
