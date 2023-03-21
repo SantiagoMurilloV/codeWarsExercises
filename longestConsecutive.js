6f44ddfd5ced3f3971490e58ccd1de80fd4f7e16
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2023-03-21 15:56:03 -0500

    Add files via upload

---
+++ longestConsecutive.js
@@ -0,0 +1,23 @@
+function longestConsec(strarr, k) {
+    if (strarr.length === 0 || k > strarr.length || k <= 0) {
+    return "";
+    }
+    
+    let longestString = "";
+    for (let i = 0; i < strarr.length-1; i++) {
+    let currentString = strarr.slice(i, i + k).join(""); // with .slice() I return a shallow copy of a slice of the array
+                                                         // with .join() concatenate the elements by means of a specific separator
+        if (currentString.length > longestString.length) {
+        longestString = currentString;
+        }
+    }
+    
+console.log(longestString);
+}
+
+
+let strarr = ['hello', 'world','time','hamburger'];
+let k = 2;
+
+longestConsec(strarr,k)
+
