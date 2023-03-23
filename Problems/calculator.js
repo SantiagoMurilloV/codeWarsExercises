a6b43422cbab169399d2b0318adabbe3d756d14a
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2023-03-23 12:32:14 -0500

    file update

--- Problems/calculator.js
+++ Problems/calculator.js
@@ -1,34 +1,46 @@
+// El problema consiste en hacer una funcion que simule una calculadora,que  
+// tenga dos parametros, el primero es el arreglo con el que se va  a operar y 
+// el segundo el numero de la operacion que l sele va aplicar al arreglo.
 
+// 1----- suma
+// 2------ resta
+// 3------ multiplicacion 
+
+// ejemplo:
+
+// calculator([1,2,3],3)
+
+// output: [2,3,2,6,3,6]
 
 function calculator(arr, k){
     const answere = [];
     if(arr.lenght === 0){
         return "";
     }else{ 
-    const mux = (a,b)=> a*b;
-    const sum = (a,b)=> a+b;
-    const res = (a,b)=> a-b;
-    arr.forEach((_,i) => {if ( i < arr.length - 1 ){
-        let result =  0;
-        switch(true){
-            case (k===1):
-                result = sum(arr[0],arr[i+1]);
-                answere.push(result );
-                break;
-            case (k===2):
-                result = res(arr[0],arr[i+1]);
-                answere.push(result );
-                break;
-            case (k===3):
-                result = mux(arr[0],arr[i+1]);
-                answere.push(result);
-                break;
-            default:
-                return 'Void!';
-    }}} )
+        const mux = (a,b)=> a*b;
+        const sum = (a,b)=> a+b;
+        const res = (a,b)=> a-b;
+        arr.forEach((_,i) => {if ( i < arr.length - 1 ){
+            let result =  0;
+            switch(true){
+                case (k===1):
+                    result = sum(arr[i],arr[i+1]);
+                    answere.push(result );
+                    break;
+                case (k===2):
+                    result = res(arr[i],arr[i+1]);
+                    answere.push(result );
+                    break;
+                case (k===3):
+                    result = mux(arr[i],arr[i+1]);
+                    answere.push(result);
+                    break;
+                default:
+                    return 'Void!';
+        }}} )
 
-}
+    }
 console.log(answere)
 }
 
-calculator([1,2,3],3)
+calculator([1,2,3],1)
--- Problems/uniqueNumber.js
+++ Problems/uniqueNumber.js
@@ -13,34 +13,12 @@
 // Input: nums = [0,1,0,1,0,1,99]
 // Output: 99
 
-
-// Constraints:
-
-// 1 <= nums.length <= 3 * 104
-// -231 <= nums[i] <= 231 - 1
-// Each element in nums appears exactly three times except for one element which appears once.
-
-
-
 function singleNumber(nums) {
     let number = 0;
+    if(nums.lenght === 0 )
     number = nums.filter(num => nums.indexOf(num) === nums.lastIndexOf(num));
+    // the .filter() returns the array that does not match the filter
     return number;
 }
 
-  console.log(singleNumber([2, 2, 2, 2, 5, 5, 5, 5, 7])); // OUTPUT: 4
-
-
-  // function uniqueNumber(arr){
-  //   let number = 0;
-    
-  //   arr.forEach((_,i) => {if(i < arr.lenght){
-  //       if(arr[i] === arr[i--] && arr[i]===arr[i++] ){
-
-  //       }  number = arr[i]
-          
-        
-  //   }
-  //   });
-  //   console.log(number)
-  // }
+  console.log(singleNumber([2, 2, 2, 2, 5, 5, 5, 5, 7])); // OUTPUT: [7]
